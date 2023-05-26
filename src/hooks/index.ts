ba35594fb0edf4198041d819b92e35a21e53d7ed
Dmitry Baryshev <dima.baryshev@live.com>
2023-05-26 19:54:24 +0300

    feat: add custom hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,29 @@
+import { useEffect, useRef } from 'react';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 import type { AppDispatch, RootState } from '../redux/store';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useClickOutside = <T extends HTMLElement>(
+  callback: () => void
+) => {
+  const ref = useRef<T>(null);
+
+  const handleClick = (event: MouseEvent) => {
+    if (ref.current && !event.composedPath().includes(ref.current)) {
+      callback();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  }, []);
+
+  return ref;
+};
